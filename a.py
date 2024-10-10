a4d69e58882fb6db8934faeb3af6db6f93bce0ff
Md. An Nahian Prince <130352614+RAAJKUMAR44@users.noreply.github.com>
2024-10-11 00:26:21 +0600

    Add files via upload

---
+++ a.py
@@ -0,0 +1,6 @@
+import streamlit as st 
+
+st.header("Welcome to My Website!")
+st.header("After a long time finally create my website")
+st.write("""This website is created by Python Language. Here i am using Python's Streamlit framework.
+         This framework is very powerfull and easy to write. I really enjoy this framework!""")
